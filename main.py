a69d4f77c15441adef04559d3c19d0ffe30ac8b8
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-04-13 18:22:00 +0100

    a

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # FastAPI app entry point
+
+from fastapi import FastAPI
+from database import engine, Base
+
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"status": "Clearance is live"}
