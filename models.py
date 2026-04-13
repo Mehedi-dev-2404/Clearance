c87b06c06b9c35ce4643fb206d5dcbb4880365bc
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-04-13 17:21:07 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
 # SQLAlchemy table definitions
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
