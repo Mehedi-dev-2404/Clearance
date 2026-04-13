eacc6e099b856346bf648c82e1e859ffc9aa004d
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-04-13 17:30:53 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -7,9 +7,9 @@ class UserCreate(BaseModel):
 
 class AccountCreate(BaseModel):
     user_id: int
-    balance: float
+    balance: int
 
 class TransactionCreate(BaseModel):
     from_account_id: int
     to_account_id: int
-    amount: float
+    amount: int
